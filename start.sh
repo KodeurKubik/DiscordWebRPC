1a51cfe1d45bb753fb7204a01c4c3c48568079c8
Kubik <84036232+TotoroGaming@users.noreply.github.com>
2023-02-21 18:02:35 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+clear
+
+if ! command -v python3 &> /dev/null; then
+    echo "Python3 is not installed." >&2
+    exit 1
+fi
+
+echo "Installing every dependency..."
+python3 -m pip install colorama
+python3 -m pip install pypresence
+clear
+python3 index.py
